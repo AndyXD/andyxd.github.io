bd8c448ffdb581d56036baa16a075b4ecb059480
Andy <andyboydston@gmail.com>
2020-03-07 09:32:46 -0800

    mobile animation

--- js/app.js
+++ js/app.js
@@ -3,4 +3,5 @@ const navLinks = document.querySelector(".nav-links");
 
 hamburger.addEventListener("click", () => {
     navLinks.classList.toggle("open");
+    hamburger.classList.toggle("open");
 });
